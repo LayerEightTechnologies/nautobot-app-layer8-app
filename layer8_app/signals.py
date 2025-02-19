8eabd3b7f9a4660d05fc49e1b1425dade4b75a6a
Jonathan Nathanson <jonathan.nathanson@layereight.io>
2025-02-19 16:32:55 +0000

    fix: use update_or_create correctly in signals

--- layer8_app/signals.py
+++ layer8_app/signals.py
@@ -9,30 +9,40 @@ def create_custom_fields(sender, apps, **kwargs):
 def create_default_locationtypes(sender, apps, **kwargs):
     """Create default location types for Layer8 App."""
     LocationTypes = apps.get_model("dcim", "LocationType")
-    Building = LocationTypes.objects.get_or_create(
+    ContentType = apps.get_model("contenttypes", "ContentType")
+
+    ct_strings = [
+        "dcim.device",
+        "circuits.circuit",
+        "circuits.circuittermination",
+        "ipam.namespace",
+        "ipam.prefix",
+        "ipam.vlan",
+        "ipam.vlangroup",
+    ]
+
+    cts = []
+    for ct_str in ct_strings:
+        app_label, model_name = ct_str.split(".")
+        try:
+            ct_obj = ContentType.objects.get(app_label=app_label, model=model_name)
+        except:
+            print(f"Content Type {ct_str} not found.")
+            continue
+        cts.append(ct_obj)
+
+    building, _ = LocationTypes.objects.update_or_create(
         name="Building",
-        nestable=True,
-        content_types=[
-            "dcim.device",
-            "circuits.circuit",
-            "circuits.circuit_termination",
-            "ipam.namespace",
-            "ipam.prefix",
-            "ipam.vlan",
-            "ipam.vlan_group",
-        ],
+        defaults={
+            "nestable": True,
+        },
     )
-    LocationTypes.objects.get_or_create(
+    building.content_types.set(cts)
+    room, _ = LocationTypes.objects.update_or_create(
         name="Room",
-        nestable=True,
-        content_types=[
-            "dcim.device",
-            "circuits.circuit",
-            "circuits.circuit_termination",
-            "ipam.namespace",
-            "ipam.prefix",
-            "ipam.vlan",
-            "ipam.vlan_group",
-        ],
-        parent=Building,
+        defaults={
+            "nestable": True,
+            "parent": building,
+        },
     )
+    room.content_types.set(cts)
